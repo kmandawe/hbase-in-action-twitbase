529071701136af14c3eab7951a201fae6f24c191
kmandawe <kdmandawe06@yahoo.com.ph>
2021-07-25 17:13:23 +0800

    Reading all data of row

---
+++ src/main/java/com/kensbunker/hbaseinaction/twitbase/_2_2_4_ReadingData.java
@@ -0,0 +1,23 @@
+package com.kensbunker.hbaseinaction.twitbase;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.io.IOException;
+
+public class _2_2_4_ReadingData {
+  public static void main(String[] args) throws IOException {
+    // Opening a connection
+    // Create a connection to the cluster.
+    Configuration conf = HBaseConfiguration.create();
+    try (Connection connection = ConnectionFactory.createConnection(conf);
+        Table table = connection.getTable(TableName.valueOf("users"))) {
+      Get g = new Get(Bytes.toBytes("TheRealMT"));
+      Result r = table.get(g);
+      System.out.println(r);
+    }
+  }
+}
