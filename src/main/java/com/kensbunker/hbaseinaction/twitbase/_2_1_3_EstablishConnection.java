c1008f3420c2902ab1ad8875a4f6ec21eccf3f5c
kmandawe <kdmandawe06@yahoo.com.ph>
2021-07-25 16:39:55 +0800

    Initial commit

---
+++ src/main/java/com/kensbunker/hbaseinaction/twitbase/_2_1_3_EstablishConnection.java
@@ -0,0 +1,23 @@
+package com.kensbunker.hbaseinaction.twitbase;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Table;
+
+import java.io.IOException;
+
+public class _2_1_3_EstablishConnection {
+  public static void main(String[] args) throws IOException {
+    // Opening a connection
+    // Create a connection to the cluster.
+    Configuration conf = HBaseConfiguration.create();
+    try (Connection connection = ConnectionFactory.createConnection(conf);
+        Table table = connection.getTable(TableName.valueOf("users"))) {
+      // use table as needed, the table returned is lightweight
+      System.out.println(table.getDescriptor());
+    }
+  }
+}
