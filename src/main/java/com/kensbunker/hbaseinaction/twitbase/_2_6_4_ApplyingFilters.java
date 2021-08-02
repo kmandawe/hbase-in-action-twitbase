935a652659e7ac67a348172335d8ea1ccc70e98b
kmandawe <kdmandawe06@yahoo.com.ph>
2021-08-02 10:13:25 +0800

    Applying Filters using ParseFilter

--- src/main/java/com/kensbunker/hbaseinaction/twitbase/_2_6_4_ApplyingFilters.java
+++ src/main/java/com/kensbunker/hbaseinaction/twitbase/_2_6_4_ApplyingFilters.java
@@ -1,22 +1,17 @@
 package com.kensbunker.hbaseinaction.twitbase;
 
-import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.CompareOperator;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.*;
-import org.apache.hadoop.hbase.filter.CompareFilter;
 import org.apache.hadoop.hbase.filter.Filter;
+import org.apache.hadoop.hbase.filter.ParseFilter;
 import org.apache.hadoop.hbase.filter.RegexStringComparator;
 import org.apache.hadoop.hbase.filter.ValueFilter;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Arrays;
 
 public class _2_6_4_ApplyingFilters {
   public static void main(String[] args) throws IOException {
@@ -30,17 +25,23 @@ public class _2_6_4_ApplyingFilters {
 
       Scan s = new Scan();
       s.addColumn(Bytes.toBytes("twits"), Bytes.toBytes("twit"));
-      Filter f = new ValueFilter(CompareOperator.EQUAL, new RegexStringComparator(".*TwitBase.*"));
+
+      // Using ValueFilter
+//      Filter f = new ValueFilter(CompareOperator.EQUAL, new RegexStringComparator(".*TwitBase.*"));
+
+      // Using ParseFilter
+      String expression = "ValueFilter(=,'regexstring:.*TwitBase.*')";
+      ParseFilter p = new ParseFilter();
+      Filter f = p.parseSimpleFilterExpression(Bytes.toBytes(expression));
+
       s.setFilter(f);
       ResultScanner rs = table.getScanner(s);
-
 
       for (Result r : rs) {
         // extract the twit
         byte[] twitBytes = r.getValue(Bytes.toBytes("twits"), Bytes.toBytes("twit"));
         String message = Bytes.toString(twitBytes);
         System.out.println("Twit: " + message);
-
       }
     }
   }
